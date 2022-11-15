19ee3bf48d551a5233818e26260d1becb1fc36e8
b1910319 <ldtrinh2001@gmail.com>
2022-11-15 10:51:19 +0700

    thống kê khi đã xử lý đơn hàng

--- app/models/donhangM.php
+++ app/models/donhangM.php
@@ -46,7 +46,7 @@ use Carbon\Carbon;
       return $this->db->select($sql);
     }
     public function doanhthu_homnay ($table_dh, $ngay){
-      $sql = "SELECT ngaylap_dh, tonggia_dh , SUM(tonggia_dh) AS tong FROM $table_dh WHERE $table_dh.ngaylap_dh = '$ngay'";
+      $sql = "SELECT ngaylap_dh, tonggia_dh , SUM(tonggia_dh) AS tong FROM $table_dh WHERE $table_dh.ngaylap_dh = '$ngay'AND $table_dh.tinhtrang_dh != 0";
       return $this->db->select($sql);
     }
     public function doanhthu_homnay_nv ($table_dh, $ngay, $dieukien){
@@ -82,7 +82,7 @@ use Carbon\Carbon;
       return $this->db->select($sql);
     }
     public function soluong_ngay($table_dh, $table_sp, $table_ctdh){
-      $sql = "SELECT * , SUM(soluong_dat) AS tongban_ngay FROM $table_ctdh join $table_dh on $table_dh.ma_dh = $table_ctdh.ma_dh join $table_sp on $table_ctdh.ma_sp = $table_sp.ma_sp   GROUP BY ngaylap_dh ";
+      $sql = "SELECT * , SUM(soluong_dat) AS tongban_ngay FROM $table_ctdh join $table_dh on $table_dh.ma_dh = $table_ctdh.ma_dh join $table_sp on $table_ctdh.ma_sp = $table_sp.ma_sp where $table_dh.tinhtrang_dh != 0  GROUP BY ngaylap_dh ";
       return $this->db->select($sql);
     }
     public function soluong_ngay_nv($table_dh, $table_sp, $table_ctdh, $dieukien){
@@ -90,7 +90,7 @@ use Carbon\Carbon;
       return $this->db->select($sql);
     }
     public function soluong_thang($table_dh, $table_sp, $table_ctdh){
-      $sql = "SELECT * , SUM(soluong_dat) AS tongban_thang FROM $table_ctdh join $table_dh on $table_dh.ma_dh = $table_ctdh.ma_dh join $table_sp on $table_ctdh.ma_sp = $table_sp.ma_sp  GROUP BY thanglap_dh ";
+      $sql = "SELECT * , SUM(soluong_dat) AS tongban_thang FROM $table_ctdh join $table_dh on $table_dh.ma_dh = $table_ctdh.ma_dh join $table_sp on $table_ctdh.ma_sp = $table_sp.ma_sp where $table_dh.tinhtrang_dh != 0 GROUP BY thanglap_dh ";
       return $this->db->select($sql);
     }
     public function soluong_thang_nv($table_dh, $table_sp, $table_ctdh, $dieukien){
@@ -98,7 +98,7 @@ use Carbon\Carbon;
       return $this->db->select($sql);
     }
     public function soluong_nam($table_dh, $table_sp, $table_ctdh){
-      $sql = "SELECT * , SUM(soluong_dat) AS tongban_nam FROM $table_ctdh join $table_dh on $table_dh.ma_dh = $table_ctdh.ma_dh join $table_sp on $table_ctdh.ma_sp = $table_sp.ma_sp  GROUP BY namlap_dh ";
+      $sql = "SELECT * , SUM(soluong_dat) AS tongban_nam FROM $table_ctdh join $table_dh on $table_dh.ma_dh = $table_ctdh.ma_dh join $table_sp on $table_ctdh.ma_sp = $table_sp.ma_sp where $table_dh.tinhtrang_dh != 0 GROUP BY namlap_dh ";
       return $this->db->select($sql);
     }
     public function soluong_nam_nv($table_dh, $table_sp, $table_ctdh, $dieukien){
@@ -106,7 +106,7 @@ use Carbon\Carbon;
       return $this->db->select($sql);
     }
     public function tongtien_ngay($table_dh){
-      $sql = "SELECT * , SUM($table_dh.tonggia_dh) AS tongtien_ngay FROM $table_dh GROUP BY ngaylap_dh";
+      $sql = "SELECT * , SUM($table_dh.tonggia_dh) AS tongtien_ngay FROM $table_dh where $table_dh.tinhtrang_dh != 0 GROUP BY ngaylap_dh";
       return $this->db->select($sql);
     }
     public function tongtien_ngay_nv($table_dh, $dieukien){
@@ -114,11 +114,11 @@ use Carbon\Carbon;
       return $this->db->select($sql);
     }
     public function tongtien_thang($table_dh){
-      $sql = "SELECT * , SUM($table_dh.tonggia_dh) AS tongtien_thang FROM $table_dh GROUP BY thanglap_dh";
+      $sql = "SELECT * , SUM($table_dh.tonggia_dh) AS tongtien_thang FROM $table_dh where $table_dh.tinhtrang_dh != 0 GROUP BY thanglap_dh";
       return $this->db->select($sql);
     }
     public function tongtien_nam($table_dh){
-      $sql = "SELECT * , SUM($table_dh.tonggia_dh) AS tongtien_nam FROM $table_dh GROUP BY namlap_dh";
+      $sql = "SELECT * , SUM($table_dh.tonggia_dh) AS tongtien_nam FROM $table_dh where $table_dh.tinhtrang_dh != 0 GROUP BY namlap_dh";
       return $this->db->select($sql);
     }
     public function tongtien_thang_nv($table_dh, $dieukien){
@@ -130,7 +130,7 @@ use Carbon\Carbon;
       return $this->db->select($sql);
     }
     public function count_sp_ngay($table_dh, $table_ctdh, $table_sp){
-      $sql = "SELECT *, SUM($table_ctdh.soluong_dat) as soluong FROM $table_dh join $table_ctdh on $table_dh.ma_dh = $table_ctdh.ma_dh join $table_sp on $table_ctdh.ma_sp = $table_sp.ma_sp  GROUP BY $table_ctdh.ma_sp, $table_dh.ngaylap_dh ORDER BY $table_dh.ngaylap_dh desc , soluong desc ";
+      $sql = "SELECT *, SUM($table_ctdh.soluong_dat) as soluong FROM $table_dh join $table_ctdh on $table_dh.ma_dh = $table_ctdh.ma_dh join $table_sp on $table_ctdh.ma_sp = $table_sp.ma_sp where $table_dh.tinhtrang_dh != 0 GROUP BY $table_ctdh.ma_sp, $table_dh.ngaylap_dh ORDER BY $table_dh.ngaylap_dh desc , soluong desc ";
       return $this->db->select($sql);
     }
     public function count_sp_ngay_nv($table_dh, $table_ctdh, $table_sp, $dieukien){
@@ -138,7 +138,7 @@ use Carbon\Carbon;
       return $this->db->select($sql);
     }
     public function count_sp_thang($table_dh, $table_ctdh, $table_sp){
-      $sql = "SELECT *, SUM($table_ctdh.soluong_dat) as soluong FROM $table_dh join $table_ctdh on $table_dh.ma_dh = $table_ctdh.ma_dh join $table_sp on $table_ctdh.ma_sp = $table_sp.ma_sp  GROUP BY $table_ctdh.ma_sp, $table_dh.thanglap_dh ORDER BY $table_dh.thanglap_dh desc , soluong desc ";
+      $sql = "SELECT *, SUM($table_ctdh.soluong_dat) as soluong FROM $table_dh join $table_ctdh on $table_dh.ma_dh = $table_ctdh.ma_dh join $table_sp on $table_ctdh.ma_sp = $table_sp.ma_sp where $table_dh.tinhtrang_dh != 0 GROUP BY $table_ctdh.ma_sp, $table_dh.thanglap_dh ORDER BY $table_dh.thanglap_dh desc , soluong desc ";
       return $this->db->select($sql);
     }
     public function count_sp_thang_nv($table_dh, $table_ctdh, $table_sp, $dieukien){
@@ -146,7 +146,7 @@ use Carbon\Carbon;
       return $this->db->select($sql);
     }
     public function count_sp_nam($table_dh, $table_ctdh, $table_sp){
-      $sql = "SELECT *, SUM($table_ctdh.soluong_dat) as soluong FROM $table_dh join $table_ctdh on $table_dh.ma_dh = $table_ctdh.ma_dh join $table_sp on $table_ctdh.ma_sp = $table_sp.ma_sp  GROUP BY $table_ctdh.ma_sp, $table_dh.namlap_dh ORDER BY $table_dh.namlap_dh desc , soluong desc ";
+      $sql = "SELECT *, SUM($table_ctdh.soluong_dat) as soluong FROM $table_dh join $table_ctdh on $table_dh.ma_dh = $table_ctdh.ma_dh join $table_sp on $table_ctdh.ma_sp = $table_sp.ma_sp where $table_dh.tinhtrang_dh != 0 GROUP BY $table_ctdh.ma_sp, $table_dh.namlap_dh ORDER BY $table_dh.namlap_dh desc , soluong desc ";
       return $this->db->select($sql);
     }
     public function count_sp_nam_nv($table_dh, $table_ctdh, $table_sp, $dieukien){
